36c6b85d9e61058fcf74fad0074f0d202daffb68
Henrik Hose <henrik.s.hose@googlemail.com>
2020-12-15 16:16:39 +0100

    changed imu covariance to ros standard

--- include/bno055_usb_stick/decoder.hpp
+++ include/bno055_usb_stick/decoder.hpp
@@ -65,12 +65,12 @@ public:
     imu.linear_acceleration = output.acceleration;
 
     // To indicate no covariance estimate, set the 1st elements of matrice -1
-    imu.orientation_covariance[0] = -1;
+    imu.orientation_covariance[0] = 0;
     std::fill(imu.orientation_covariance.begin() + 1, imu.orientation_covariance.end(), 0.);
-    imu.angular_velocity_covariance[0] = -1;
+    imu.angular_velocity_covariance[0] = 0;
     std::fill(imu.angular_velocity_covariance.begin() + 1, imu.angular_velocity_covariance.end(),
               0.);
-    imu.linear_acceleration_covariance[0] = -1;
+    imu.linear_acceleration_covariance[0] = 0;
     std::fill(imu.linear_acceleration_covariance.begin() + 1,
               imu.linear_acceleration_covariance.end(), 0.);
 
